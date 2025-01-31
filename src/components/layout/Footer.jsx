fde68f0e9789ed23d1760d8787e8a583bdf6a86a
DrHardyc <dr_hard_84@mail.ru>
2025-01-31 18:30:45 +0300

    - Центрировал формы логина и регистрации. Добавил к ним анимации. - Футер перестал быть статичным.

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Footer() {
     return (
-        <footer className="fixed bottom-0 left-0 w-full bg-white text-gray-800 py-4 hidden sm:block">
+        <footer className="w-full bg-white text-gray-800 py-4 hidden sm:block">
             <div className="container mx-auto text-center">
                 <p>&copy; 2025 FastFood. Все права защищены.</p>
                 <p>
